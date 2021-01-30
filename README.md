8d1989a3fc278a17a3c055002ec6021908e593e5
sgalibert <stephane.galibert.perso@outlook.com>
2021-01-30 17:47:06 +0100

    Fix trevis

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 [![Build Status](https://travis-ci.com/stephgalibert/ECGE.svg?token=4xRtKLGfqqmAj18wsve3&branch=main)](https://travis-ci.com/stephgalibert/ECGE)
 
 # ECGE
+
 Entity Component Game Engine
 
 ## Dependencies
 
-* https://github.com/erincatto/box2d/tree/v2.4.1   
-* https://github.com/skypjack/entt/tree/v3.6.0
-    * copy the src folder into `/usr/include`
+* [Box2D](https://github.com/erincatto/box2d/tree/v2.4.1)   
+* [Entt](https://github.com/skypjack/entt/tree/v3.6.0)
   
 ## Build
 
-* mkdir build && cd build
-* cmake ..
+* `scripts/deps.sh`
+* `scripts/build.sh`
 
 ## Run
 
--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,7 +1,17 @@
+#!/bin/sh
+
 # Installs box2d deps
-sudo apt-get install xorg-dev libglu1-mesa-dev -y
+sudo apt-get -y install xorg-dev libglu1-mesa-dev
 
 # Installs box2d
 git clone --depth 1 --branch v2.4.1 https://github.com/erincatto/box2d.git
-cd box2d && mkdir build && cd build
-cmake ../ && sudo make install && cd ../../
+cd box2d && mkdir build && cd build && cmake ../ && sudo make install
+cd ../../
+
+# Installs entt
+git clone --depth 1 --branch v3.6.0 https://github.com/skypjack/entt.git
+echo "Copied 'src/entt' to '/usr/include'"
+cd entt && sudo cp -r src/entt /usr/include
+
+# Installs project deps
+sudo apt-get -y install libsfml-dev
